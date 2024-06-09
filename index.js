0197664b47f29ebe36649b40f1a3cf120ba6c98a
ibrahimakiluibrahim <ibrahimakiluibrahim@gmail.com>
2024-06-09 02:42:38 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+console.log("hello ibrahim")
+
+
+//variables
+var firstname = 'ahmad';
+let lastname = "ibrahim";
+let x, y, z;
+let a, b, c;
+let o = y + z;
+a=6
+b=6
+c=a+b
+x=5;
+y=2;
+z=x+y;
+
+console.log(x)
+console.log(firstname)
+console.log(lastname)
+console.log(y)
+console.log(z)
+console.log(a)
+console.log(b)
+console.log(c)
+
